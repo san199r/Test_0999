5d20d2119f67cd853f55e35e9bb47f9f63d552c6
Bob <psantosh1999@gmail.com>
2026-05-20 19:48:03 +0530

    Added project files

--- LehighAcres_City_FL_Permits_DateRange_Tool.py
+++ LehighAcres_City_FL_Permits_DateRange_Tool.py
@@ -257,6 +257,11 @@ def setup_driver():
         options.add_argument("--headless=new")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--disable-gpu")
+        options.add_argument("--window-size=1920,1080")
+        options.add_argument("--disable-extensions")
+        options.add_argument("--disable-software-rasterizer")
+        options.add_argument("--remote-allow-origins=*")
     else:
         options.add_argument("--start-maximized")
 
