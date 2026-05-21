f558301acc93cf4b7a5c6f3f8bea63c7a7fc8342
Santosh Pepakayala <psantosh1999@gmail.com>
2026-05-21 07:51:31 +0530

    Update default END_DATE to 05/15/2026

--- LehighAcres_City_FL_Permits_DateRange_Tool.py
+++ LehighAcres_City_FL_Permits_DateRange_Tool.py
@@ -42,7 +42,7 @@ SEARCH_INPUT_ID = "ctl00_PlaceHolderMain_generalSearchForm_txtGSPermitNumber"
 SEARCH_BUTTON_XPATH = '//*[@id="ctl00_PlaceHolderMain_btnNewSearch"]'
 
 START_DATE = os.getenv("START_DATE", "04/26/2026")
-END_DATE = os.getenv("END_DATE", "04/31/2026")
+END_DATE = os.getenv("END_DATE", "05/15/2026")
 
 DATE_FROM_SELECTORS = [
     (By.ID, "ctl00_PlaceHolderMain_generalSearchForm_txtGSFromDate"),
